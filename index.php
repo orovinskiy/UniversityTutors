86a61050644ce5ba3990ed9e32cdcdc8530ce38b
olegr <orovinskiy@mail.greenriver.edu>
2020-04-25 15:33:12 -0700

    Added a route to a view called checklist. created a list of check boxes and set up the main headers.

--- index.php
+++ index.php
@@ -20,6 +20,11 @@ session_start();
 //create an instance of the base class
 $f3 = Base::instance();
 
+//define variables
+$f3->set("tutorForms",array("ADP Registration","Adult Sexual Misconduct",
+    "Affirmations and Disclosures","Handbook Verification","I-9(first step)","Offer Letter",
+    "Orientation RSVP","W4"));
+
 // Define a default route
 $f3->route('GET /', function () {
     $view = new Template();
@@ -31,5 +36,12 @@ $f3->route('GET /test', function () {
     $view = new Template();
     echo $view->render("views/home.html");
 });
+
+// Define a route too tutors checklist
+$f3->route('GET /checklist', function () {
+    $view = new Template();
+    echo $view->render("views/checklist.html");
+});
+
 
 $f3->run();
