bdc0a3bd52392424ce037976f74664038dcc5126
laxmi kandel <55778064+laxmikandel97@users.noreply.github.com>
2020-04-26 14:51:56 -0400

    gave some styling to the form

---
+++ scripts/formScript.js
@@ -0,0 +1,19 @@
+const inputFile = document.getElementById("img");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+inputFile.addEventListener("change", function () {
+    const file = this.files[0];
+    // console.log(file);
+    if (file) {
+        const reader = new FileReader();
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+        reader.addEventListener("load", function () {
+            previewImage.setAttribute("src", this.result);
+
+        });
+        reader.readAsDataURL(file);
+    }
+});
