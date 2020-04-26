8c1694ac652e1d2d8636eb7742834b3e9f65916f
kellerflint <kflint0068@gmail.com>
2020-04-26 10:06:49 -0700

    commeting script

--- scripts/tutors_script.js
+++ scripts/tutors_script.js
@@ -1,3 +1,9 @@
+/**
+ * Scripts for the tutors.html onboarding management page
+ * @author Keller Flint
+ */
+
+// Creates tutors DataTable
 $(document).ready(function () {
     $('#tutors-table').DataTable({
         responsive: true
