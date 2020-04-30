15a438da45943992751c370d353d211d1848b262
kellerflint <kflint0068@gmail.com>
2020-04-29 21:28:00 -0700

    tutors page populates data from database

--- controller/Controller.php
+++ controller/Controller.php
@@ -21,8 +21,21 @@ class Controller
         $this->_f3 = $f3;
     }
 
-    function tutorsPage() {
+    function tutorsPage()
+    {
         global $db;
+
+        $tutorsData = $db->getTutors("2020");
+
+        $this->_f3->set("backgroundOptions", array("none" => "Not Done", "sent" => "Sent", "clear" => "Clear", "flag" => "Flag"));
+        $this->_f3->set("referenceOptions", array("none" => "Not Done", "incomplete" => "In Progress", "clear" => "Clear", "flag" => "Flag"));
+        $this->_f3->set("ADPOptions", array("none" => "Not Sent", "invited" => "Invited", "registered" => "Registered"));
+        $this->_f3->set("i9Options", array("none" => "Not Sent", "tutor" => "Tutor Done", "admin" => "Admin Done"));
+
+        $this->_f3->set("tutorsData", $tutorsData);
+
+        var_dump($tutorsData);
+
         $view = new Template();
         echo $view->render("views/tutors.html");
     }
