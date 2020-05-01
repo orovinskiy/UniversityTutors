811595ec0c2d3c54d1a146cac6c32bfaf9106b58
kellerflint <kflint0068@gmail.com>
2020-05-01 14:54:11 -0700

    implemented add tutor button on tutors page

--- controller/Controller.php
+++ controller/Controller.php
@@ -33,12 +33,13 @@ class Controller
 
         // Get tutor data for current year
         $tutorsData = $this->_db->getTutors($year);
-        
+
         // Set values for select dropdowns
         $this->_f3->set("backgroundOptions", array("none" => "Not Done", "sent" => "Sent", "clear" => "Clear", "flag" => "Flag"));
         $this->_f3->set("referenceOptions", array("none" => "Not Done", "incomplete" => "In Progress", "clear" => "Clear", "flag" => "Flag"));
         $this->_f3->set("ADPOptions", array("none" => "Not Sent", "invited" => "Invited", "registered" => "Registered"));
         $this->_f3->set("i9Options", array("none" => "Not Sent", "tutor" => "Tutor Done", "admin" => "Admin Done"));
+        $this->_f3->set("year", $year);
 
         // Store tutor data is hive
         $this->_f3->set("tutorsData", $tutorsData);
@@ -51,7 +52,14 @@ class Controller
      * Ajax logic for tutors page
      * @author Keller Flint
      */
-    function tutorsAjax() {
-        $this->_db->updateYearData($_POST["column"], $_POST["value"], $_POST["yearId"]);
+    function tutorsAjax()
+    {
+        if (isset($_POST["yearId"])) {
+            $this->_db->updateYearData($_POST["column"], $_POST["value"], $_POST["yearId"]);
+        } else if (isset($_POST["year"])) {
+            // TODO create function to generate and send email to tutor
+            echo $this->_db->addNewTutor($_POST["year"], $_POST["email"]);
+        }
+
     }
 }
