771d815431b89e05c8ae85dc0fdbf731d3e8eb4c
dsloan92 <dsloan4@mail.greenriver.edu>
2020-05-11 21:26:45 -0700

    created user object

--- classes/user.php
+++ classes/user.php
@@ -2,33 +2,49 @@
 
 /**
  * User class for University Tutors Project
+ * Creates a new user by using the userEmail obtained upon login
  * @author Dallas Sloan
  */
 
 class User
 {
-    private $userEmail;
     private $userIsAdmin;
-    private $userName;
+    private $userID;
     private $_db;
 
     /**
      * User constructor.
-     * @param $userEmail
-     * @param $userIsAdmin
-     * @param $userName
-     * @param $_db
+     * @param $userEmail email of user logging into system
+     * @param $_db database object
+     * @author Dallas Sloan
      */
-    public function __construct($userID, $userYear, $_db)
+    public function __construct($userEmail, $_db)
     {
         $this->_db = $_db;
-        $userInfo = $this->_db->getTutor($userYear, $userID);
-        $this->userEmail = $userInfo["user_email"];
-        //$this->userName = $userInfo['tutor_first'] + userInfo['tutor_last'];
-        //$this->userIsAdmin = $userInfo['']
-        var_dump($userInfo);
-        //echo $this->userName;
-        //var_dump($userInfo["user_email"]);
+        $userInfo = $this->_db->getUserByEmail($userEmail);
+        $this->userID = $userInfo['user_id'];
+        $this->userIsAdmin = $userInfo['user_is_admin'];
+        //var_dump($userInfo); //used for testing
+    }
+
+    /**
+     * Getter for userIsAdmin field
+     * @return int 1 if user is admin 0 if not
+     * @author Dallas Sloan
+     */
+    public function getUserIsAdmin()
+    {
+        return $this->userIsAdmin;
+    }
+
+    /**
+     * Getter for userID field
+     * @return int user ID
+     * @author Dallas Sloan
+     */
+    public function getUserID()
+    {
+        return $this->userID;
     }
 
 
