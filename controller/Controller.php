8f25272305301f44947f5b35ab29130698168084
kellerflint <kflint0068@gmail.com>
2020-05-15 15:28:04 -0700

    Added tutor info page

--- controller/Controller.php
+++ controller/Controller.php
@@ -344,4 +344,16 @@ class Controller
         $view = new Template();
         echo $view->render('views/admin.html');
     }
+
+    /**
+     * @param array $param Param array containing the id of the tutor we want to load
+     */
+    function tutorInfoPage($param) {
+
+        $this->_f3->set("tutor", $this->_db->getTutorById($param["id"]));
+        $this->_f3->set("user", $this->_db->getUserById($param["id"]));
+
+        $view = new Template();
+        echo $view->render('views/tutorInfo.html');
+    }
 }
