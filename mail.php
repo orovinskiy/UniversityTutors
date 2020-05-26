3c7b1472d6bf55b62981006930521dcc2f100ba3
dsloan92 <dsloan4@mail.greenriver.edu>
2020-05-26 09:58:25 -0700

    Updated the mail file to be an mail object. Created a template email json file so I could save default body and subject information

--- mail.php
+++ mail.php
@@ -1,56 +1,150 @@
 <?php
 require "PHPMailer/PHPMailerAutoload.php";
 
-/**
- * Function that creates and sends an email with an attachment optional
- * @param String $to recipient of email
- * @param String $from Sender of email
- * @param String $from_name Name of sender
- * @param String $subject Subject line of email
- * @param String $body Body of email, just be in HTML format
- * @return bool true if email was sent successfully, false otherwise
- * @throws phpmailerException
- * @author Dallas Sloan and GitHub
- */
-function smtpmailer($to, $from, $from_name, $subject, $body)
-{
-    //instantiating new mailer object
-    $mail = new PHPMailer();
-    $mail->IsSMTP();
-    $mail->SMTPAuth = true;
+class Mail {
+    private $_subject;
+    private $_body;
+    private $_defaultAttachments;
+    private $_JSONData;
 
-    $mail->SMTPSecure = 'ssl';
-    $mail->Host = 'kold-tutors.greenriverdev.com';
-    $mail->Port = 465;
-    $mail->Username = 'universitytutors@kold-tutors.greenriverdev.com';
-    $mail->Password = 'Monday!99';
-
-    //adding attachments to email
-    //$path = 'attachments/IT355.zip';
-    //$path2 = 'attachments/PyramidMod.png';
-    //$mail->AddAttachment($path);
-    //$mail->AddAttachment($path2);
-
-    //email information
-    $mail->IsHTML(true);
-    $mail->From = "universitytutors@kold-tutors.greenriverdev.com";
-    $mail->FromName = $from_name;
-    $mail->Sender = $from;
-    $mail->AddReplyTo($from, $from_name);
-    $mail->Subject = $subject;
-    $mail->Body = $body;
-    $mail->AddAddress($to);
-    //checking whether or not email was successful
-    if (!$mail->Send()) {
-        //$error ="Please try Later, Error Occurred while Processing...";
-        //return $error;
-        $success = false;
-        return $success;
-    } else {
-        //$error = "Thanks You!! Your email is sent.";
-        //return $error;
-        $success = true;
-        return $success;
+    /**
+     * Mail constructor which pulls default subject, body, and attachments from emailTemplate.json
+     */
+    public function __construct()
+    {
+        //pulling in the JSON file to be read and decoding info
+        $JSONFile = 'emailTemplate.json';
+        $this->_JSONData = file_get_contents($JSONFile);
+        $this->_JSONData = json_decode($this->_JSONData, true);
+        $this->_subject = $this->_JSONData['subject'];
+        $this->_body = $this->_JSONData['body'];
+        $this->_defaultAttachments = $this->_JSONData['attachment'];
     }
+
+    /**
+     * Getter for subject field
+     * @return String default subject of email
+     */
+    public function getSubject()
+    {
+        return $this->_subject;
+    }
+
+    /**
+     * Setter of subject field. Will re-write JSON file accordingly
+     * @param String $subject default subject of email
+     */
+    public function setSubject($subject)
+    {
+        $this->_subject = $subject;
+        //saving new subject to JSONData variable and re-writing to json
+        $this->_JSONData['subject'] = $subject;
+        //encoding
+        $updatedJSON = json_encode($this->_JSONData);
+        file_put_contents('emailTemplate.json', $updatedJSON);
+    }
+
+    /**
+     * Getter for default email body
+     * @return String default body for email
+     */
+    public function getBody()
+    {
+        return $this->_body;
+    }
+
+    /**
+     * setter of body field for email. Will re-write JSON file accordingly
+     * @param String $body default body of email
+     */
+    public function setBody($body)
+    {
+        $this->_body = $body;
+        //saving new subject to JSONData variable and re-writing to json
+        $this->_JSONData['body'] = $body;
+        //encoding
+        $updatedJSON = json_encode($this->_JSONData);
+        file_put_contents('emailTemplate.json', $updatedJSON);
+    }
+
+    /**
+     * Getter for default attachments for email
+     * @return String default name of attachment
+     */
+    public function getDefaultAttachments()
+    {
+        return $this->_defaultAttachments;
+    }
+
+    /**
+     * setter of default attachments for email. Will re-write JSON file accordingly
+     * @param String $defaultAttachments path for file attachment
+     */
+    public function setDefaultAttachments($defaultAttachments)
+    {
+        $this->_defaultAttachments = $defaultAttachments;
+        //saving new subject to JSONData variable and re-writing to json
+        $this->_JSONData['attachment'] = $defaultAttachments;
+        //encoding
+        $updatedJSON = json_encode($this->_JSONData);
+        file_put_contents('emailTemplate.json', $updatedJSON);
+
+    }
+
+    /**
+     * Function that creates and sends an email with an attachment optional
+     * @param String $to recipient of email
+     * @param String $from Sender of email
+     * @param String $from_name Name of sender
+     * @param String $subject Subject line of email
+     * @param String $body Body of email, just be in HTML format
+     * @return bool true if email was sent successfully, false otherwise
+     * @throws phpmailerException
+     * @author Dallas Sloan and GitHub
+     */
+    function smtpmailer($to, $from, $from_name, $loginBody)
+    {
+        //instantiating new mailer object
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true;
+
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'kold-tutors.greenriverdev.com';
+        $mail->Port = 465;
+        $mail->Username = 'universitytutors@kold-tutors.greenriverdev.com';
+        $mail->Password = 'Monday!99';
+
+        //adding attachments to email
+        //$path = 'attachments/IT355.zip';
+        //$path2 = 'attachments/PyramidMod.png';
+        //$mail->AddAttachment($path);
+        //$mail->AddAttachment($path2);
+
+        //email information
+        $mail->IsHTML(true);
+        $mail->From = $from;
+        $mail->FromName = $from_name;
+        $mail->Sender = $from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $this->_subject;
+        $mail->Body = "<p>" . $this->_body . "</p>" . "<p>$loginBody</p>";
+        $mail->AddAddress($to);
+        //checking whether or not email was successful
+        if (!$mail->Send()) {
+            //$error ="Please try Later, Error Occurred while Processing...";
+            //return $error;
+            $success = false;
+            return $success;
+        } else {
+            //$error = "Thanks You!! Your email is sent.";
+            //return $error;
+            $success = true;
+            return $success;
+        }
+    }
+
+
 }
+
 
