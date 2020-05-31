978eb05d43b6cb56cff731cbb2ac81339365f3c7
dsloan92 <dsloan4@mail.greenriver.edu>
2020-05-30 21:00:27 -0700

    Updated the mail function to delete files from the upload folder when being removed from the JSON

--- mail.php
+++ mail.php
@@ -79,17 +79,30 @@ class Mail {
     /**
      * setter of default attachments for email. Will re-write JSON file accordingly
      * @param String $defaultAttachments path for file attachment
+     * @return int returns whether the file is saved (1), the file is not saved (0) or if the file is not saved due
+     * to the file alraedy existing (2)
      */
     public function setDefaultAttachments($defaultAttachments)
     {
         //add attachment to array
+        //first check to see if attachment already exists
+        $attachmentArray = $this->_JSONData['attachment'];
+        for ($i = 0; $i < count($attachmentArray); $i++) {
+            if ($attachmentArray[$i] == $defaultAttachments) {
+                return 2; //2 indicates that file already exists
+            }
+        }
+
         array_push($this->_defaultAttachments, $defaultAttachments);
         //$this->_defaultAttachments = $defaultAttachments;
         //saving new subject to JSONData variable and re-writing to json
         $this->_JSONData['attachment'] = $this->_defaultAttachments;
         //encoding
         $updatedJSON = json_encode($this->_JSONData);
-        file_put_contents('emailTemplate.json', $updatedJSON);
+        if (file_put_contents('emailTemplate.json', $updatedJSON)) {
+            return 1;
+        } else return 0;
+
 
 
     }
@@ -100,21 +113,24 @@ class Mail {
         for ($i = 0; $i < count($attachmentArray); $i++) {
             if ($attachmentArray[$i] == $attachment) {
                 unset($attachmentArray[$i]);
-                $deleted = true;
-            }
-            else{
+                if (unlink($attachment)) {
+                    $deleted = true;
+                } else {
+                    $deleted = "File not deleted from directoy";
+                }
+            } else{
                 $deleted = false;
             }
         }
         //normalize integer values
-        array_values($attachmentArray);
+        $attachmentArray = array_values($attachmentArray);
         //saving updates to JSON file
         $this->_JSONData['attachment'] = $attachmentArray;
         //encode
         $updatedJSON = json_encode($this->_JSONData);
         file_put_contents('emailTemplate.json', $updatedJSON);
-        //return $deleted;
-        return $attachmentArray;
+        return $deleted;
+        //return $attachmentArray;
     }
 
     /**
@@ -140,6 +156,15 @@ class Mail {
         $mail->Port = 465;
         $mail->Username = 'universitytutors@kold-tutors.greenriverdev.com';
         $mail->Password = 'Monday!99';
+
+        //check to see if there are any attachments to be added if there are, add attachment to email
+        if (!empty($this->_defaultAttachments))
+        {
+            for ($i = 0; $i < count($this->_defaultAttachments); $i++)
+            {
+                $mail->addAttachment($this->_defaultAttachments[$i]);
+            }
+        }
 
         //adding attachments to email
         //$path = 'attachments/IT355.zip';
